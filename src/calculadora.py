d21df7a6b2241a1ea7cb075d3ccf9c036ae5240f
Daniel Sanchez <danielssj88@gmail.com>
2024-02-24 12:41:25 -0500

    HERE

--- src/calculadora.py
+++ src/calculadora.py
@@ -13,6 +13,7 @@ def calcular(numero1, numero2, operacion):
         case '/':
             # Operación división
             respuesta = numero1 / numero2
+        HERE
         case _:
             raise ValueError("Operación inválida.")
 
